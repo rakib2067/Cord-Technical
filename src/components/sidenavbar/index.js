760a466034c4ebfe09ad881269a7d48b756acc13
rakib2067 <rakib7@hotmail.co.uk>
2022-06-22 22:48:49 +0100

    side bar styling changes (padding) according to wireframe

--- src/components/sidenavbar/index.js
+++ src/components/sidenavbar/index.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 import { NavLink as Link } from "react-router-dom";
 
 import * as colors from "../../colors";
 import Arrow from "../../images/arrow-icon.png";
 import SearchWhite from "../../images/search-icon-white.png";
 
-export default function SideNavBar () {
+export default function SideNavBar() {
   const [isOpen, setIsOpen] = useState(false);
   /* TODO: Write the necessary functions to open and close the sidebar */
 
   return (
-    <SideNavBarCont className={isOpen ? 'visible' : ''}>
+    <SideNavBarCont className={isOpen ? "visible" : ""}>
       {/* TODO: Implement a hamburger icon that controls the open state of the sidebar. This control should only be visible on mobile devices via CSS media queries */}
       {/* The sidebar should slide in from left */}
       <SideNavHeader>
@@ -22,10 +22,14 @@ export default function SideNavBar () {
         Discover
         <img src={SearchWhite} alt="Magnifying glass" />
       </SideNavMainLink>
-      <SideNavSectionTitle><HeaderText>Watched</HeaderText></SideNavSectionTitle>
+      <SideNavSectionTitle>
+        <HeaderText>Watched</HeaderText>
+      </SideNavSectionTitle>
       <NavLink to="/watched/movies">Movies</NavLink>
       <NavLink to="/watched/tv-shows">Tv Shows</NavLink>
-      <SideNavSectionTitle><HeaderText>Saved</HeaderText></SideNavSectionTitle>
+      <SideNavSectionTitle>
+        <HeaderText>Saved</HeaderText>
+      </SideNavSectionTitle>
       <NavLink to="/saved/movies">Movies</NavLink>
       <NavLink to="/saved/tv-shows">Tv Shows</NavLink>
     </SideNavBarCont>
@@ -35,18 +39,18 @@ export default function SideNavBar () {
 const SideNavBarCont = styled.div`
   position: fixed;
   z-index: 9;
-  width: 280px;
+  width: 260px;
   height: 100%;
   background-color: ${colors.sideNavBar};
   color: white;
-`
+`;
 
 const SectionsStyles = css`
   position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 25px 35px;
+  padding: 35px 35px 25px 35px;
   font-size: 1.6em;
   font-weight: 700;
   color: white;
@@ -72,27 +76,28 @@ const SideNavSectionTitle = styled.div`
   font-size: 1.6em;
   font-weight: 700;
   padding: 25px 0 15px 35px;
-`
+`;
 
 const HeaderText = styled.div`
   padding: 0 35px 10px 0;
   border-bottom: 1px solid ${colors.lightBackground};
-`
+`;
 
 const NavLink = styled(Link)`
   display: block;
   color: white;
-  opacity: .8;
+  opacity: 0.8;
   font-size: 1.2em;
   padding: 10px 35px;
 
-  &:hover, &:focus-visible {
+  &:hover,
+  &:focus-visible {
     opacity: 1;
     background: ${colors.sideNavBarHover};
   }
 
-  &.active { 
+  &.active {
     background: ${colors.primaryColor};
     opacity: 1;
   }
-`
+`;
