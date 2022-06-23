891e9b39e8be01b6312c916aa25b7ba371f13c3e
rakib2067 <rakib7@hotmail.co.uk>
2022-06-23 19:22:12 +0100

    lots of styling changes, added styled components to use in the movieitem component

--- src/components/movieitem/index.js
+++ src/components/movieitem/index.js
@@ -1,24 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 
+import { sideNavBar, primaryColor } from "../../colors";
+
 export default function MovieItem({ movie, genres }) {
   genres = genres.filter((genre) => {
     return movie.genre_ids.some((id) => id == genre.id);
   });
-
+  let genresLength = genres.length - 1;
   return (
     // TODO: Complete the MovieItem component
 
     <MovieItemWrapper>
       <LeftCont>
-        <img src={movie.url} alt="" />
+        <MoviePoster src={movie.url} alt="" />
       </LeftCont>
       <RightCont>
-        <Title>{movie.title}</Title>
-        <p>Genres: {genres.map((genre) => genre.name)}</p>
-        <p>Release: {movie.release_date}</p>
-        <p>Rating: {movie.vote_average}</p>
-        <p>Overview: {movie.overview}</p>
+        <MovieHeader>
+          <Title>{movie.title}</Title>
+          <Rating>{movie.vote_average}</Rating>
+        </MovieHeader>
+        <GenresContainer>
+          {genres.map((genre, index) => {
+            if (index == 0) return <Genre>{genre.name} |</Genre>;
+            else if (index == genresLength) return <Genre> {genre.name}</Genre>;
+            else return <Genre> {genre.name} |</Genre>;
+          })}
+        </GenresContainer>
+
+        <Description>{movie.overview}</Description>
+        {/* <p>Release: {movie.release_date}</p> */}
       </RightCont>
     </MovieItemWrapper>
   );
@@ -26,20 +37,53 @@ export default function MovieItem({ movie, genres }) {
 
 const MovieItemWrapper = styled.div`
   position: relative;
+  display: flex;
   background-color: white;
   border-radius: 5px;
   padding: 20px;
   margin: 15px 0;
 `;
 
-const LeftCont = styled.div`
-  display: inline-block;
+const MovieHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
+const MoviePoster = styled.img`
+  height: 100%;
+`;
+
+const GenresContainer = styled.div`
+  margin-bottom: 15px;
+`;
+
+const Genre = styled.span`
+  color: ${primaryColor};
+  font-weight: bold;
+`;
+
+const LeftCont = styled.div``;
+
 const RightCont = styled.div`
-  display: inline-block;
+  padding-left: 20px;
 `;
 
 const Title = styled.h2`
-  font-size: 1.4;
+  font-size: 1.7rem;
+  margin: 0;
+  font-weight: 900;
+  color: ${sideNavBar};
+`;
+
+const Rating = styled.span`
+  background: ${primaryColor};
+  color: #fff;
+  border-radius: 5px;
+  padding: 5px 8px;
+  align-self: center;
+  font-weight: bold;
+`;
+
+const Description = styled.p`
+  margin: 0;
 `;
